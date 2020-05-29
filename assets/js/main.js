70f4143fb629e937b9675d8a9adc553e027a726d
PetrKhyznyak <k2wow2@gmail.com>
2020-05-30 02:48:44 +0300

    second

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,64 @@
-let myNum = 10;
-console.log(myNum);
-var myStr = 'строка';
-console.log(myStr)
-var myBool = true;
-console.log(myBool);
-var myArr = [1, 2, 3, 4, 5];
-console.log(myArr);
-var myObj = ['first Name', 'Last Name'];
-console.log(myObj);
+ var myNum = 10;
+ console.log(myNum);
+ var myStr = 'строка';
+ console.log(myStr)
+ var myBool = true;
+ console.log(myBool);
+ var myArr = [1, 2, 3, 4, 5];
+ console.log(myArr);
+ var myObj = {
+  first: 'First Name',
+  last: 'Last Name'
+ };
+ console.log(myObj);
 
-var decimal2 = myNum.toFixed(2);
-console.log(decimal2);
+ var decimal2 = myNum.toFixed(2);
+ console.log(decimal2);
 
 
-var i = 1;
-console.log(++i);
-console.log(i);
-console.log(i++);
-console.log(i);
+ var i = 1;
+ console.log(i++);
+ console.log(++i);
+ console.log(i);
+ console.log(i--);
+ console.log(--i);
+ console.log(i);
 
-var myTest = 20;
-myTest += myNum;
-console.log(myTest);
-myTest -= myNum;
-console.log(myTest);
-myTest *= myNum;
-console.log(myTest);
-myTest %= myNum;
-console.log(myTest);
-myTest /= myNum;
-console.log(myTest);
 
-var myPi = Math.PI;
-console.log(myPi);
-var myRound = Math.round(89.279);
-console.log(myRound);
-var myRandom = Math.random()*10;
-console.log(myRandom);
-var myPow = Math.pow(3,5);
-console.log(myPow);
 
-var strObj = {str: 'Мама мыла раму, рама мыла маму'};
-strObj.length = strObj.str.length;
+ var myTest = 20;
+ myTest += myNum;
+ console.log(myTest);
+ myTest -= myNum;
+ console.log(myTest);
+ myTest *= myNum;
+ console.log(myTest);
+ myTest %= myNum;
+ console.log(myTest);
+ myTest /= myNum;
+ console.log(myTest);
 
-console.log(strObj);
-var isRamaPos = strObj.str.indexOf('рама');
-console.log('rama position:', isRamaPos);
+ var myPi = Math.PI;
+ console.log(myPi);
+ var myRound = Math.round(89.279);
+ console.log(myRound);
+ var myRandom = Math.random()*10;
+ console.log(myRandom);
+ var myPow = Math.pow(3,5);
+ console.log(myPow);
 
-var strReplace = strObj.str.replace('рама', 'Рама');
- strReplace = strReplace.replace('моет', 'мыла');
- strReplace = strReplace.replace('мыла', 'дер');
+ var strObj = {str: 'Мама мыла раму, рама мыла маму'};
+ strObj.length = strObj.str.length;
 
-console.log(strReplace);
+ console.log(strObj);
+ var isRamaPos = strObj.str.indexOf('рама');
+ console.log('rama position:', isRamaPos);
 
-console.log(strReplace.toUpperCase());
-console.log(strReplace.toLowerCase());
+ var strReplace = strObj.str.replace('мыла', 'моет');
+  strReplace = strReplace.replace('рама', 'Рама');
+  strReplace = strReplace.replace('мыла', 'держит');
+
+ console.log(strReplace);
+
+ console.log(strReplace.toUpperCase());
+ console.log(strReplace.toLowerCase());
